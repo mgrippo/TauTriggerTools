3666cad6c93cee5e3168f1781a955abc4eeb8761
Konstantin <konstantin.androsov@gmail.com>
2019-09-21 13:09:55 +0200

    Bugfixes in crab_cmd.py.

--- Common/scripts/crab_cmd.py
+++ Common/scripts/crab_cmd.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # Execute crab command for multiple tasks.
 
-import os
 import argparse
+import os
+import subprocess
+import sys
 
 parser = argparse.ArgumentParser(description='Execute crab command for multiple tasks.',
                   formatter_class = lambda prog: argparse.HelpFormatter(prog,width=90))
@@ -14,7 +16,7 @@ args = parser.parse_args()
 if args.cmd == 'submit':
     print("ERROR: Please, use crab_submit.py to run the submit command.")
     sys.exit(1)
-cmd_args_str = ' '.join(cmd_args)
+cmd_args_str = ' '.join(args.cmd_args)
 
 def sh_call(cmd):
     sep = '-' * (len(cmd) + 3)
@@ -23,11 +25,10 @@ def sh_call(cmd):
     if result != 0:
         print('ERROR: failed to run "{}"'.format(cmd))
         sys.exit(1)
-    print(sep)
 
-for dir in os.listdir(options.workArea):
-    task_dir = os.path.join(options.workArea, dir)
+for dir in os.listdir(args.workArea):
+    task_dir = os.path.join(args.workArea, dir)
     if not os.path.isdir(task_dir): continue
 
-    cmd_line = 'crab {} -d {} {}'.format(args.cmd, args.workArea, cmd_args_str)
+    cmd_line = 'crab {} -d {} {}'.format(args.cmd, task_dir, cmd_args_str)
     sh_call(cmd_line)
